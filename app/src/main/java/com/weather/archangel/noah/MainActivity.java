67755fa95c66642ebaf4a652f820b500f0769080
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2019-04-07 14:57:45 +0200

    Add ListView and Item

--- app/src/main/java/com/weather/archangel/noah/MainActivity.java
+++ app/src/main/java/com/weather/archangel/noah/MainActivity.java
@@ -16,19 +16,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
-
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
     }
 
+    /**
+     * Inflating the Menu Option when the activity created
+     *
+     * @param menu menu to inflate
+     * @return boolean value indicating the succession or falling of the inflation process
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
+    /**
+     * get the item id from the option menu and then do an operation
+     * @param item MenuItem that is selected
+     * @return boolean value indicating the succession or falling of the process
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
